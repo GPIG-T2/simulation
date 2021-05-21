08779d58aa93f7261bcab7a38816a31d470561c6
Jack Radforth <jack@radforth.co.uk>
2021-05-21 16:03:50 +0100

    Created basic who layout

---
+++ WHO/Tracking/BasicTrigger.cs
@@ -0,0 +1,109 @@
+﻿using Models;
+using Serilog.Debugging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WHO.Extensions;
+
+namespace WHO.Tracking
+{
+    /// <summary>
+    /// Used to call the ResultingAction when the ComparisonFunction returns true, in the case of the
+    /// BasicTrigger it only accepts either LESS_THAN or GREATER_THAN to check whether the percentage
+    /// change of a parameter over the given timespan has changed by more or less than the threshold.
+    /// </summary>
+    class BasicTrigger : ITrigger
+    {
+
+        private readonly TrackingValue _parameter;
+        private readonly int _timespan;
+        private readonly Action _resultingAction;
+        private readonly (int, int) _depthRange;
+        private readonly TrackingFunction _comparisonFunction;
+        private readonly float _threshold;
+
+        public TrackingValue Parameter => this._parameter;
+
+        public int Timespan => this._timespan;
+
+        public Action ResultingAction => this._resultingAction;
+
+        public (int, int) DepthRange => this._depthRange;
+
+        public TrackingFunction ComparisonFunction => this._comparisonFunction;
+
+        public float Threshold => this._threshold;
+
+        public BasicTrigger(TrackingValue parameter, TrackingFunction comparisonFunction, float threshold, Action resultingAction, int timespan, (int, int)? depthRange = null)
+        {
+            this._parameter = parameter;
+            this._comparisonFunction = comparisonFunction;
+            this._threshold = threshold;
+            this._resultingAction = resultingAction;
+            this._timespan = timespan;
+            this._depthRange = depthRange ?? (-1, -1);
+        }
+
+        public void Apply(LocationTracker tracker)
+        {
+            int currentLastestTimestamp = tracker.Count - 1;
+            int currentEarliestTimestamp = currentLastestTimestamp - this.Timespan;
+
+            int previousLatestTimestamp = currentEarliestTimestamp - 1;
+            int previousEarliestTimestamp = previousLatestTimestamp - this.Timespan;
+
+            if (previousEarliestTimestamp < 0)
+            {
+                return;
+            }
+
+            InfectionTotals current;
+            InfectionTotals previous;
+
+            if (this.Timespan == 0)
+            {
+                current = tracker.Get(currentLastestTimestamp);
+                previous = tracker.Get(previousLatestTimestamp);
+            }
+            else
+            {
+                current = tracker.GetSum(currentEarliestTimestamp, currentLastestTimestamp);
+                previous = tracker.GetSum(previousEarliestTimestamp, previousLatestTimestamp);
+            }
+
+            float currentPercentage = this.GetPercentageForInfectionTotals(current);
+            float previousPercentage = this.GetPercentageForInfectionTotals(previous);
+
+            float change = currentPercentage / previousPercentage;
+            bool evaluation = false;
+            switch (this.ComparisonFunction)
+            {
+                case TrackingFunction.GREATER_THAN:
+                    evaluation = change > this.Threshold;
+                    break;
+                case TrackingFunction.LESS_THAN:
+                    evaluation = change < this.Threshold;
+                    break;
+            }
+
+            if (evaluation)
+            {
+                this.ResultingAction.Invoke();
+            }
+
+        }
+
+
+        protected float GetPercentageForInfectionTotals(InfectionTotals totals)
+        {
+            int total = totals.GetTotalPeople();
+
+            int parameter = totals.GetParameterTotals(this.Parameter);
+
+            return (float)parameter / total;
+        }
+
+    }
+}
---
+++ WHO/Tracking/ITrigger.cs
@@ -0,0 +1,55 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHO.Tracking
+{
+    interface ITrigger
+    {
+        /// <summary>The parameter that will be checked</summary>
+        public TrackingValue Parameter { get; }
+
+        /// <summary>The timespan over which the values are calculated in number of days</summary> 
+        public int Timespan { get; }
+
+        /// <summary>Action that will be called when the conditions are met</summary>
+        public Action ResultingAction { get; }
+
+        /// <summary>The range of location depths that the function will be on. (-1, -1) will be used to represent all depths. First value inclusive second value is exclusive.</summary> 
+        public (int, int) DepthRange { get; }
+
+        /// <summary>
+        /// Checks if the tracked location matches the conditions and then executes the ResultingAction
+        /// </summary>
+        /// <param name="tracker">The tracked location to check</param>
+        public void Apply(LocationTracker tracker);
+
+        /// <summary>
+        /// Checks whether the depth is within the range given in the trigger definition
+        /// </summary>
+        /// <param name="depth">The depth that is being checked</param>
+        /// <returns>true if the depth is within the range [a, b) otherwise false</returns>
+        public bool IsValidDepth(int depth)
+        {
+            // This means no depth checks
+            if (this.DepthRange == (-1, -1))
+            {
+                return true;
+            }
+
+            if (this.DepthRange.Item1 == -1)
+            {
+                return depth < this.DepthRange.Item2;
+            }
+            else if (this.DepthRange.Item2 == -1)
+            {
+                return depth >= this.DepthRange.Item1;
+            }
+            return depth >= this.DepthRange.Item1 && depth < this.DepthRange.Item2;
+        }
+
+    }
+}
