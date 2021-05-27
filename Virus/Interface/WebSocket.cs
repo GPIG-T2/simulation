b8acf55404b5a0e13553d52953fb3c871b5f3720
Luna <luna@luvies.io>
2021-05-27 18:20:44 +0100

    Added base server interface

---
+++ Virus/Interface/WebSocket.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace Virus.Interface
+{
+    public class WebSocket : IServer
+    {
+        private readonly WebSocketServer _server = new();
+        private bool _init = false;
+
+        public void Start(IHandler handler)
+        {
+            if (!this._init)
+            {
+                this._server.AddWebSocketService<InterfaceBehaviour>("/", b => { b.Init(handler); });
+                this._init = true;
+            }
+
+            this._server.Start();
+        }
+
+        public void Stop()
+        {
+            this._server.Stop(CloseStatusCode.Normal, "Server shutdown");
+        }
+
+        public void Dispose()
+        {
+            if (this._server.IsListening)
+            {
+                this.Stop();
+            }
+        }
+
+        private class InterfaceBehaviour : WebSocketBehavior
+        {
+#pragma warning disable CS8618 // We know these fields are not-null here
+            private IHandler _handler;
+#pragma warning restore CS8618
+
+            public void Init(IHandler handler)
+            {
+                this._handler = handler;
+            }
+
+            protected override async void OnMessage(MessageEventArgs e)
+            {
+                // Check invariants.
+                if (!e.IsText)
+                {
+                    Console.Error.WriteLine("ERROR: WebSocket message is not text");
+                    return;
+                }
+
+                // Deserialise message.
+                var request = JsonSerializer.Deserialize<Models.WebSocket.Request>(e.Data);
+                if (request == null)
+                {
+                    Console.Error.WriteLine("ERROR: Failed to parse message");
+                    return;
+                }
+
+                // Handle message.
+                var response = await this.HandleMessage(request);
+
+                // Serialise and respond.
+                string data = JsonSerializer.Serialize(response);
+                this.Send(data);
+            }
+
+            private Task<Models.WebSocket.Response> HandleMessage(Models.WebSocket.Request request)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
---
+++ Virus/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Models;
+
+namespace Virus
+{
+    public class Program : Interface.IHandler, IDisposable
+    {
+        private readonly Interface.IServer _server = new Interface.WebSocket();
+        private World _world;
+
+        public Program()
+        {
+            // TODO: properly init world
+        }
+
+        public void Start()
+        {
+            this._server.Start(this);
+        }
+
+        public void Stop()
+        {
+            this._server.Stop();
+        }
+
+        public Task<List<ActionResult>> ApplyActions(List<WhoAction> actions)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SimulationStatus> EndTurn()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Actor>> GetInfoActor(List<int> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ActorSearchResult>> GetInfoActors(SearchRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ActorSearchResult>> GetInfoHomes(SearchRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<TestResults>> GetInfoTestResults(SearchRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<InfectionTotals>> GetInfoTotals(SearchRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SimulationSettings> GetSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<SimulationStatus> GetStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            this._server.Dispose();
+        }
+    }
+}
